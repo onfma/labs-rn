fe407d77820dc513bff6b885842b68a0b4b18d60
Maria Onofrei <94899775+onfma@users.noreply.github.com>
2023-12-04 15:38:25 +0200

    Update tema2.py

--- tema2.py
+++ tema2.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
-from torchvision import datasets, transforms
 import requests
 import gzip
 import numpy as np
