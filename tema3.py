75ab4f3563c9def9f1c18ad89b99e0aa492ebe79
Maria Onofrei <94899775+onfma@users.noreply.github.com>
2024-01-17 21:06:57 +0200

    tema3 - v3

--- tema3.py
+++ tema3.py
@@ -5,49 +5,43 @@ import numpy as np
 import flappy_bird_gymnasium
 import gymnasium
 
-env = gymnasium.make("FlappyBird-v0", render_mode="human")
-
 class QNetwork(nn.Module):
     def __init__(self, input_size, output_size):
         super(QNetwork, self).__init__()
-        self.fc = nn.Linear(input_size, 128)
+        self.reshape = nn.Flatten()
+        self.fc1 = nn.Linear(input_size, 256)
         self.relu = nn.ReLU()
-        self.output = nn.Linear(128, output_size)
+        self.fc2 = nn.Linear(256, output_size)
 
     def forward(self, x):
-        x = self.fc(x)
+        x = self.reshape(x)
+        x = self.fc1(x)
         x = self.relu(x)
-        return self.output(x)
+        return self.fc2(x)
 
 class QLearningAgent:
-    def __init__(self, state_size, action_size, gamma=0.99):
+    def __init__(self, state_size, action_size, gamma=0.99, epsilon=1.0, epsilon_decay=0.99, epsilon_min=0.01):
         self.q_network = QNetwork(state_size, action_size)
         self.target_network = QNetwork(state_size, action_size)
         self.target_network.load_state_dict(self.q_network.state_dict())
         self.target_network.eval()
 
-        self.optimizer = optim.Adam(self.q_network.parameters(), lr=0.001)
+        self.optimizer = optim.Adam(self.q_network.parameters(), lr=0.0001)
         self.loss_fn = nn.MSELoss()
 
         self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
 
     def select_action(self, state):
-        q_values = self.q_network(state)
-        player_position = state[0][9].item()
-        last_top_pipe_position = state[0][1].item()
-        last_bottom_pipe_position = state[0][2].item()
+        if torch.rand(1).item() < self.epsilon:
+            return torch.randint(0, 2, (1,)).item()  # Explore
+        else:
+            with torch.no_grad():
+                q_values = self.q_network(state)
+                return torch.argmax(q_values).item()  # Exploit
 
-        # if player_position < 0.1: 
-        #     return 0  # NU sari
-        # else:
-        #     if player_position > last_top_pipe_position:
-        #         print("last_top_pipe_position")
-        #         return 1 # sari
-        #     if player_position < last_bottom_pipe_position:
-        #         print("last_bottom_pipe_position")
-        #         return 0  # NU sari
-        return torch.argmax(q_values).item()  # Exploatare
-                
     def update_q_network(self, state, action, reward, next_state, done):
         self.optimizer.zero_grad()
 
@@ -68,10 +62,11 @@ class QLearningAgent:
         self.target_network.load_state_dict(self.q_network.state_dict())
 
 # Hyperparametri
-state_size = env.observation_space.shape[0]
-action_size = env.action_space.n
-epsilon = 0.1
+state_size = 12
+action_size = 2
 episodes = 1000
+
+env = gymnasium.make("FlappyBird-v0", render_mode="human")
 
 agent = QLearningAgent(state_size, action_size)
 
@@ -81,10 +76,7 @@ for episode in range(episodes):
     total_reward = 0
 
     while True:
-        if np.random.rand() < epsilon:
-            action = np.random.choice([0, 1])
-        else:
-            action = agent.select_action(state)
+        action = agent.select_action(state)
         next_state, reward, done, _, score = env.step(action)
         next_state = torch.tensor(next_state, dtype=torch.float32).view(1, -1) /255.0
         reward = 0.1 if not done else -1.0 if done and total_reward < 1.0 else 1.0
@@ -96,7 +88,8 @@ for episode in range(episodes):
 
         if done:
             agent.update_target_network()
-            print(f"Episode: {episode + 1}, Total Reward: {total_reward}, Score: {score}")
+            agent.epsilon = max(agent.epsilon * agent.epsilon_decay, agent.epsilon_min)
+            print(f"Episode: {episode + 1}, Epsilon: {agent.epsilon}, Total Reward: {total_reward}, Score: {score}")
             break
 
 env.close()
